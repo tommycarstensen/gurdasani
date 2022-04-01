ea181c7e2dfb7b21116039b88d89315703270635
tommycarstensen <github@tommycarstensen.com>
2022-04-01 14:23:04 +0200

    Added cross validation for hyperparameter optimization.

--- run_vader.py
+++ run_vader.py
@@ -13,110 +13,155 @@ import os
 # W_train = np.load('W.npy')
 # save_path = os.path.join('out_vader', 'vader.ckpt_cell_type_LSTM_recurrent_True')
 
-X_train = np.load('X_granular.npy')
-W_train = np.load('W_granular.npy')
-save_path = os.path.join('out_vader', 'vader.ckpt_cell_type_LSTM_recurrent_True_granular')
+def main():
 
-# exclude years with missingness greater than 40%
-W_train = W_train[:,1999-1940:2016-1940+1,:]
-X_train = X_train[:,1999-1940:2016-1940+1,:]
+    print('loading X_granular.npy')
+    X_train = np.load('X_granular.npy')
+    print('loading W_granular.npy')
+    W_train = np.load('W_granular.npy')
 
-for k in range(2,6):
+    save_path = os.path.join('out_vader', 'vader.ckpt_cell_type_LSTM_recurrent_True_granular')
 
- if os.path.isfile('c_granular.{}.npy'.format(k)):
-  continue
+    # exclude years with missingness greater than 40%
+    W_train = W_train[:,1999-1940:2016-1940+1,:]
+    X_train = X_train[:,1999-1940:2016-1940+1,:]
 
- vader = VADER(
-    k=k,
-    X_train=X_train,
-    W_train=W_train,
-    save_path=save_path,
-    # n_hidden=[12, 2],
-    # k=4,
-    # learning_rate=1e-3,
-    # output_activation=None,
-    # recurrent=True,
-    # batch_size=64,
-    # recurrent=False,
-    # batch_size=16,
+    # simple(save_path, X_train, W_train)
 
-    # cell_type="LSTM",
-    # recurrent=True,
+    # https://en.wikipedia.org/wiki/Cross-validation_(statistics)#k-fold_cross-validation
+    k_cross_validation = 3
+    hyperparameter_optimization(
+        save_path, X_train, W_train,
+        k_cross_validation,
+        )
 
-    #           # learning_rate=1e-3, output_activation=None, recurrent=True, cell_type="Transformer", batch_size=64,
-    #           # cell_params={'d_model': 4, 'num_layers': 1, 'num_heads': 1, 'dff': 16, 'rate': 0.0},
-    )
+    return
 
- vader.pre_fit(n_epoch=50, verbose=True)
 
- vader.fit(n_epoch=50, verbose=True)
+def simple(save_path, X_train, W_train):
 
- print('_reconstruction_loss', vader._reconstruction_loss)
- print('_latent_loss', vader._latent_loss)
- print('reconstruction_loss', vader.reconstruction_loss)
- print('latent_loss', vader.latent_loss)
- print('get_loss', vader.get_loss(X_train, W_train))
- print('loss', vader.loss)
+    for k in range(2,6):
+     if k != 3: continue
 
- c = vader.cluster(X_train)
- with open('c_granular.{}.npy'.format(k), 'wb') as f:
-    np.save(f, c)
+     if os.path.isfile('c_granular.{}.npy'.format(k)):
+      continue
 
- p = vader.predict(X_train)
- with open('p_granular.{}.npy'.format(k), 'wb') as f:
-    np.save(f, p)
+     vader = VADER(
+        k=k,
+        X_train=X_train,
+        W_train=W_train,
+        save_path=save_path,
+        # n_hidden=[12, 2],
+        # k=4,
+        # learning_rate=1e-3,
+        # output_activation=None,
+        # recurrent=True,
+        # batch_size=64,
+        # recurrent=False,
+        # batch_size=16,
 
-print(save_path)
-print("model = keras.models.load_model('{}') ; dir(model)".format(save_path))
+        # cell_type="LSTM",
+        # recurrent=True,
 
-exit()
+        #           # learning_rate=1e-3, output_activation=None, recurrent=True, cell_type="Transformer", batch_size=64,
+        #           # cell_params={'d_model': 4, 'num_layers': 1, 'num_heads': 1, 'dff': 16, 'rate': 0.0},
+        )
 
-for k in range(1, 12 + 1):
-    # for num_layers in (1, 2):
-    for batch_size in (16, 32, 64, 128):
-        for learning_rate in (1e-4, 1e-3, 1e-2):
-            for n_hidden in ([12,2], [36,4], [36,8]):
+     vader.pre_fit(n_epoch=50, verbose=True)
 
-                vader = VADER(
-                    X_train=X_train,
-                    W_train=W_train,
-                    save_path=save_path,
-                    # n_hidden=[12, 2],
-                    # num_layers=num_layers,
-                    k=k,
-                    learning_rate=learning_rate,
-                    # output_activation=None,
-                    # recurrent=True,
-                    batch_size=batch_size,
-                    # recurrent=False,
-                    # batch_size=16,
+     vader.fit(n_epoch=50, verbose=True)
 
-                    # cell_type="LSTM",
-                    # recurrent=True,
+     print('_reconstruction_loss', vader._reconstruction_loss)
+     print('_latent_loss', vader._latent_loss)
+     print('reconstruction_loss', vader.reconstruction_loss)
+     print('latent_loss', vader.latent_loss)
+     print('get_loss', vader.get_loss(X_train, W_train))
+     print('loss', vader.loss)
 
-                    #           # learning_rate=1e-3, output_activation=None, recurrent=True, cell_type="Transformer", batch_size=64,
-                    #           # cell_params={'d_model': 4, 'num_layers': 1, 'num_heads': 1, 'dff': 16, 'rate': 0.0},
-                    )
+     c = vader.cluster(X_train)
+     with open('c_granular.{}.npy'.format(k), 'wb') as f:
+        np.save(f, c)
 
-                vader.pre_fit(n_epoch=50, verbose=True)
+     p = vader.predict(X_train)
+     with open('p_granular.{}.npy'.format(k), 'wb') as f:
+        np.save(f, p)
 
-                vader.fit(n_epoch=50, verbose=True)
+    print(save_path)
+    print("model = keras.models.load_model('{}') ; dir(model)".format(save_path))
 
-                with open('get_loss.txt', 'a') as f:
-                    d = vader.get_loss(X_train, W_train)
-                    print(
-                        k, batch_size, learning_rate,
-                        n_hidden[0], n_hidden[1],
-                        d['reconstruction_loss'], d['latent_loss'],
-                        sep='\t', file=f,
-                        )
+    exit()
 
-                c = vader.cluster(X_train)
-                with open(os.path.join(save_path, 'c.npy'), 'wb') as f:
-                    np.save(f, c)
+    return
 
-                p = vader.predict(X_train)
-                with open(os.path.join(save_path, 'p.npy'), 'wb') as f:
-                    np.save(f, p)
 
-                                    
+def hyperparameter_optimization(
+    save_path, X, W,
+    k_cross_validation,
+    ):
+
+    sample_size = X.shape[0] // k_cross_validation
+    for i_cross_validation in range(k_cross_validation):
+        print('i_cross_validation', i_cross_validation)
+        i1 = (i_cross_validation + 0) * sample_size
+        i2 = (i_cross_validation + 1) * sample_size
+        X_train = X[i1:i2,:,:]
+        W_train = W[i1:i2,:,:]
+        # k: Number of mixture components. (default: 3)
+        for k in range(1, 12 + 1):
+            print('k', k)
+            # for num_layers in (1, 2):
+            # Batch size used for training. (default: 32)
+            for batch_size in (16, 32, 64, 128):
+                # Learning rate for training. (default: 1e-3)
+                for learning_rate in (1e-4, 1e-3, 1e-2):
+                    # The hidden layers. List of length >= 1. Specification of the number of nodes in the hidden layers.
+                    for n_hidden in ([12,2], [36,4], [36,8]):
+
+                        vader = VADER(
+                            X_train=X_train,
+                            W_train=W_train,
+                            save_path=save_path,
+                            # n_hidden=[12, 2],
+                            # num_layers=num_layers,
+                            k=k,
+                            learning_rate=learning_rate,
+                            # output_activation=None,
+                            # recurrent=True,
+                            batch_size=batch_size,
+                            # recurrent=False,
+                            # batch_size=16,
+
+                            # cell_type="LSTM",
+                            # recurrent=True,
+
+                            #           # learning_rate=1e-3, output_activation=None, recurrent=True, cell_type="Transformer", batch_size=64,
+                            #           # cell_params={'d_model': 4, 'num_layers': 1, 'num_heads': 1, 'dff': 16, 'rate': 0.0},
+                            )
+
+                        vader.pre_fit(n_epoch=50, verbose=True)
+
+                        vader.fit(n_epoch=50, verbose=True)
+
+                        with open('get_loss.txt', 'a') as f:
+                            d = vader.get_loss(X_train, W_train)
+                            print(
+                                k_cross_validation,
+                                k, batch_size, learning_rate,
+                                n_hidden[0], n_hidden[1],
+                                d['reconstruction_loss'], d['latent_loss'],
+                                sep='\t', file=f,
+                                )
+
+                        c = vader.cluster(X_train)
+                        with open(os.path.join(save_path, 'c.npy'), 'wb') as f:
+                            np.save(f, c)
+
+                        p = vader.predict(X_train)
+                        with open(os.path.join(save_path, 'p.npy'), 'wb') as f:
+                            np.save(f, p)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
